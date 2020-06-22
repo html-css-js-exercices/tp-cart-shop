a4f997d667a1a16b685c9f589920522d020b8e61
vinz neo <vinz.neo@gmail.com>
2020-06-22 09:29:42 +0200

    more files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-main.js
+/**
+ * Instruction popin :
+ * Vous devez insérer ce script dans la page panier.html
+ * Pour utiliser le script de popin vous devez mettre l'id "buttonReduction" sur le boutton "Afficher le code promo"
+ * Pour fermer la popin il faudra créer un bouton et lui attribuer l'id "closePopinReduction"
+ */
+(function(){
+	window.addEventListener('load',function(e){
+		var popin = document.getElementById('popinReduction');
+		var button = document.getElementById('buttonReduction');
+		var close = document.getElementById('closePopinReduction');
+
+		popin.style.display="none";
+
+		button.addEventListener('click',function(){
+			console.log(popin);
+			popin.style.display = "block";
+		});
+		close.addEventListener('click',function(){
+			popin.style.display = "none";
+		})
+	})
+})();
